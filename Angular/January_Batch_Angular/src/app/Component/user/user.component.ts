c94639f9cfcb2541245a397f6bf2a7a110b8a070
Jigar1992 <jigar.thakkar@tatvasoft.com>
2025-04-15 20:53:55 +0530

    Angular pushed

---
+++ Angular/January_Batch_Angular/src/app/Component/user/user.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  firstName: string = "Jigar";
+  lastName: any = [];
+  id = "txtId";
+  isAvtive = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    // const temp = 10;
+    // temp  = 15;
+
+    // let temp = 10;
+    // temp = 15;
+
+    // var and let 
+
+
+  }
+
+  onSaveClick() {
+    debugger
+    alert(1);
+  }
+
+  onMouseenter() {
+    debugger
+    alert("onMouseenter ENTER");
+  }
+
+}
+
+// x = 10;
+
+// var x;
+
+// function GetData() {
+
+
+
+//   if(true)
+//     {
+//     var x = 10;
+//     let y = 10;
+
+//   }
+//   console.log(x);
+//   console.log(y);
+
+// }
---
+++ Angular/January_Batch_Angular/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root', //Name
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'January_Batch_Angular f sfsdfsdsdf fsdffsfdfsdf sdfsdf';
+}
+
+// HTML
+// CSS
+// JS
